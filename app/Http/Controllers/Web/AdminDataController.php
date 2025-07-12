030096f17ffece72a0dda0e6214bdfeef3d8d62f
dev_vens <vensalex1991@gmail.com>
2025-07-12 20:36:37 +0200

    fix controller

--- app/Http/Controllers/Web/AdminDataController.php
+++ app/Http/Controllers/Web/AdminDataController.php
@@ -16,32 +16,32 @@ class AdminDataController extends Controller
 
         // visiteurs présents aujourd'hui
         $visits = Visit::with('visitor')
-        ->whereDate('entered_at', $today)
-        ->whereNull('exited_at')
-        ->get()
-        ->map(function ($visit) {
-            return [
-                'type' => 'visiteur',
-                'name' => $visit->visitor->first_name . ' ' . $visit->visitor->last_name,
-                'info' => $visit->visitor->email,
-                'local' => $visit->local ?? '-',
-                'time' => $visit->entered_at,
-            ];
-        });
+            ->whereDate('entered_at', $today)
+            ->whereNull('exited_at')
+            ->get()
+            ->map(function ($visit) {
+                return [
+                    'type' => 'visiteur',
+                    'name' => $visit->visitor->first_name . ' ' . $visit->visitor->last_name,
+                    'info' => $visit->visitor->email,
+                    'local' => $visit->local ?? '-',
+                    'time' => $visit->entered_at,
+                ];
+            });
 
         // formations prévues aujourd'hui
         $trainings = Training::with('staffMember')
-        ->whereDate('date', $today)
-        ->get()
-        ->map(function ($training) {
-            return [
-                'type' => 'formation',
-                'name' => $training->title,
-                'info' => $training->staffMember ? ($training->staffMember->first_name . ' ' . $training->staffMember->last_name) : '-',
+            ->whereDate('date', $today)
+            ->get()
+            ->map(function ($training) {
+                return [
+                    'type' => 'formation',
+                    'name' => $training->title,
+                    'info' => $training->staffMember ? ($training->staffMember->first_name . ' ' . $training->staffMember->last_name) : '-',
                     'local' => $training->room ?? '-',
-                'time' => $training->date,
-            ];
-        });
+                    'time' => $training->date,
+                ];
+            });
 
         // Fusion et trier par heure
         $entries = $visits->concat($trainings)->sortBy('time');
