f2c9152e7e4de32f4e970ea46f87a029d1517667
dev_vens <vensalex1991@gmail.com>
2025-07-15 09:58:25 +0200

    rebuild railway

--- build.sh
+++ build.sh
@@ -7,12 +7,17 @@ composer install --no-dev --optimize-autoloader
 npm ci
 npm run build
 
+# Create database directory and file
+mkdir -p database
+touch database/database.sqlite
+chmod 664 database/database.sqlite
+
 # Clear and cache configurations for production
+php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Create database and run migrations
-touch database/database.sqlite
+# Run migrations
 php artisan migrate --force
 php artisan db:seed --force
