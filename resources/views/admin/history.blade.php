dec247b094e7d78ac43c5ca52921ce96fbc83100
dev_vens <vensalex1991@gmail.com>
2025-07-15 20:00:55 +0200

    fix email error

--- resources/views/admin/history.blade.php
+++ resources/views/admin/history.blade.php
@@ -61,7 +61,7 @@
                     {{ $history['name'] }}
                   </td>
                   <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    {{ $history['email'] }}
+                    {{ $history['email'] ?? '-' }}
                   </td>
                   <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                     {{ $history['info'] }}
