74d6d304787b8e572ba4dee05d4165bb2de382fe
dev_vens <vensalex1991@gmail.com>
2025-07-15 14:35:52 +0200

    edit config vite

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,18 @@ export default defineConfig({
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
+            publicDirectory: "public",
         }),
     ],
     build: {
         // Ensure assets are built with correct public path
         outDir: "public/build",
         emptyOutDir: true,
-        manifest: true,
+        manifest: "manifest.json", // Put manifest directly in build folder
         rollupOptions: {
             // Don't include external dependencies in the bundle
             external: [],
         },
     },
+    base: "/build/",
 });
